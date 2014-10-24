933b8f2e9672cf975cd202654006bd174b06259f
takumi shimojo <takumin@takumi.(none)>
2014-10-24 23:10:51 +0900

    room2を構築

--- socket.js
+++ socket.js
@@ -32,20 +32,41 @@ io.on('connection',function(socket){
 
   //ルームでの動作
 
-  socket.on('join room1', function(player){
-    room1++;
-    if(room1==1){
-      //room1の参加者が１人目なら「参加待ち画面」を表示
-      console.log("room1 has "+room1+" people.");
-      io.emit('waiting',player,1);
-      socket.broadcast.emit('show enemy name',player);
-    }else if(room1==2){
-      //room1の参加者が２人目ならゲームをスタートする
-      console.log("room1 has "+room1+" people.");
-      io.emit('game start',player,1);
-      room1=0;
+  socket.on('join room', function(player,room_number){
+    if(room_number==1){
+      room1++;
+      if(room1==1){
+        //room1の参加者が１人目なら「参加待ち画面」を表示
+        console.log("room1 has "+room1+" people.");
+        io.emit('waiting',player,1);
+        socket.broadcast.emit('show enemy name',player,1);
+      }else if(room1==2){
+        //room1の参加者が２人目ならゲームをスタートする
+        console.log("room1 has "+room1+" people.");
+        io.emit('game start',player,1);
+        room1=0;
+      }
+    }else if(room_number==2){
+      room2++;
+      if(room2==1){
+        //room1の参加者が１人目なら「参加待ち画面」を表示
+        console.log("room2 has "+room2+" people.");
+        io.emit('waiting',player,2);
+        socket.broadcast.emit('show enemy name',player,2);
+      }else if(room2==2){
+        //room1の参加者が２人目ならゲームをスタートする
+        console.log("room1 has "+room2+" people.");
+        io.emit('game start',player,2);
+        room2=0;
+      }
     }
   });
+
+  //ルームにいる人に向けて自分の名前を表示させる
+  socket.on('show my name', function(jibun,room_number){
+    console.log("jibun is "+jibun+"and"+room_number);
+    socket.broadcast.emit('show my name',jibun,room_number);
+  })
   
   /* 通信を切ったとき */
   socket.on('disconnect', function(){
