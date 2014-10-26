cbbc38d4e1cf138efb771e9d34de3e9a3143f3ce
dragogonone <user@YataMac.local>
2014-10-26 17:03:27 +0900

    タイトル画面完成

--- public/javascript/title.js
+++ public/javascript/title.js
@@ -1,7 +1,10 @@
 /* タイトルでの処理に使うhtml */
 function gamemode(mode){
+	if(mode!=1){
+		alert("準備中…");
+	}else{
 	
-	/*説明テキストの変更s*/
+	/*説明テキストの変更*/
 	$("h3").text('Input your name and push "join" button.');
 	/* ゲームモード選択ボタンを削除する */
 	$(".gamemode").empty();
@@ -18,7 +21,7 @@ function gamemode(mode){
 	$("#room1").append($('<p>room1</p><form id="form1" action=""><input id="player1-1" class="input" type="text" placeholder="Your Name"><button class="OK-button">join</button><span>vs</span><span id="enemy-room1">no player</span></form>'));
 	
 	*/
-	
+	}
 }
 
 	   					
