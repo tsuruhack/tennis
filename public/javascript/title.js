abc21a91f083b83b34be54c55da1c5e2966dfd5c
dragogonone <user@YataMac.local>
2014-10-26 19:07:48 +0900

    roomの不具合途中まで

--- public/javascript/title.js
+++ public/javascript/title.js
@@ -12,7 +12,7 @@ function gamemode(mode){
 	
 	
 	
-	$("#roomquery").append($('<div class="room-box3"><li class="room"><p>room1</p><form id="form1" action=""><input id="player1-1" class="input" type="text" placeholder="Your Name"><button class="OK-button">join</button><span>vs</span><span id="enemy-room1">no player</span></form></li></div><div class="room-box3"><li class="room"><p>room2</p><form id="form2" action=""><input id="player2-1" class="input" type="text" placeholder="Your Name"><button class="OK-button">join</button><span>vs</span><span id="enemy-room2">no player</span></form></li></div>'));
+	$("#roomquery").append($('<div class=\"room-box3\"><li class=\"room\"><p>room1</p><form id=\"form1\" action=\"\"><input id=\"player1-1\" class=\"input\" type=\"text\" placeholder=\"Your Name\"><button class=\"OK-button\">join</button><span>vs</span><span id=\"enemy-room1\">no player</span></form></li></div><div class=\"room-box3\"><li class=\"room\"><p>room2</p><form id=\"form2\" action=\"\"><input id=\"player2-1\" class=\"input\" type=\"text\" placeholder=\"Your Name\"><button class=\"OK-button\">join</button><span>vs</span><span id=\"enemy-room2\">no player</span></form></li></div>'));
 	
 	//$(".ball").css({'top':'50%','left':'50%'});
 	
